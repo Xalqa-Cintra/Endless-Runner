2158b6b3328f11ba56c692a9220240a611af6b50
Xalqa-Cintra <113114207+Xalqa-Cintra@users.noreply.github.com>
2022-10-05 10:44:14 +0100

    MENU

---
+++ Assets/Scenes/MainMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+   public void LoadGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+
+    }
+}
