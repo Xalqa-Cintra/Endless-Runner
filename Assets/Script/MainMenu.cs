6d6fcf483b229d2bb5fda6a83249ee553d1b7ca8
Jude Madigan Hennessy <jmadiganhenn2022@elam.co.uk>
2022-10-10 16:49:50 +0100

    qol and menu interaction

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-   public void LoadGame()
+   public void PlayGame()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
     }
+    public void QuitGame()
+    {
+        Application.Quit();
+
+    }
+
 }
