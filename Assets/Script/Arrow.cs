9c51b1c99c24e634acf4f69a687047098af92ab5
Xalqa-Cintra <113114207+Xalqa-Cintra@users.noreply.github.com>
2022-10-12 19:22:08 +0100

    home fixes

---
+++ Assets/Script/Arrow.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+
+    Rigidbody2D playerObject;
+
+    GameObject player;
+    float playerStartPos;
+    public float speed = 0.5f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        playerObject = GetComponent<Rigidbody2D>();
+        playerStartPos = player.transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float movementValueX = -1.0f;
+        float offset = (player.transform.position.x - playerStartPos) * speed;
+
+        playerObject.velocity = new Vector2(movementValueX * speed, 0f);
+    }
+
+
+
+}
